86f7299e4c289aa247d9025394f7866eb8e2c1b8
Alexey Lyachko <alloverffs@gmail.com>
2018-04-25 12:39:18 +0300

    1.11 fix again

--- lyachko.alexey/common/circle.cpp
+++ lyachko.alexey/common/circle.cpp
@@ -5,7 +5,7 @@
 namespace lyachko
 {
 
-	Circle::Circle(const point_t & new_center, double new_r): 
+  Circle::Circle(const point_t & new_center, double new_r): 
    m_center(new_center), 
    m_r(new_r)
   {
@@ -15,34 +15,34 @@ namespace lyachko
     }
   }
 
-	double Circle::getArea() const
-	{   
-	  return double(M_PI * m_r * m_r);
-	}
+  double Circle::getArea() const
+  {
+    return double(M_PI * m_r * m_r);
+  }
 
-	rectangle_t Circle::getFrameRect() const
-	{
-	  return {m_center, m_r * 2, m_r * 2};
-	}
+  rectangle_t Circle::getFrameRect() const
+  {
+    return {m_center, m_r * 2, m_r * 2};
+  }
 
-	void Circle::move(double dx, double dy)
-	{
-	  m_center.x += dx;
-	  m_center.y += dy;
-	}
+  void Circle::move(double dx, double dy)
+  {
+    m_center.x += dx;
+    m_center.y += dy;
+  }
 
-	void Circle::move(const point_t & new_center)
-	{
-	  m_center = new_center;
-	}
+  void Circle::move(const point_t & new_center)
+  {
+    m_center = new_center;
+  }
 
-	void Circle::scale(double scale_coef)
-	{
-		if (scale_coef <= 0.0)
+  void Circle::scale(double scale_coef)
+  {
+    if (scale_coef <= 0.0)
     {
       throw std::invalid_argument("Scale coef must be greater than or even 0");
     }
-		m_r *= scale_coef;
-	}
+    m_r *= scale_coef;
+  }
 
 }
