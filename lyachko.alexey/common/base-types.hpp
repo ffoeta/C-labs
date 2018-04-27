70c6a120bda0265b98f4df29fdb44e2a0e6f98de
Alexey Lyachko <alloverffs@gmail.com>
2018-04-27 12:34:09 +0300

    namespace added for b-t

--- lyachko.alexey/common/base-types.hpp
+++ lyachko.alexey/common/base-types.hpp
@@ -1,15 +1,20 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
 
-struct point_t
+namespace lyachko
 {
-  double x, y;
-};
 
-struct rectangle_t
-{
-  point_t pos;
-  double width, height;
-};
+  struct point_t
+  {
+    double x, y;
+  };
+
+  struct rectangle_t
+  {
+    point_t pos;
+    double width, height;
+  };
+  
+}
 
 #endif 
