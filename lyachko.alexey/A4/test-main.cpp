eb9515909527bef69bd2bf9bbd5df399dbb4a99a
Alexey Lyachko <ffoeta@gmail.com>
2018-06-01 07:00:01 +0000

    Update test-main.cpp

--- lyachko.alexey/A4/test-main.cpp
+++ lyachko.alexey/A4/test-main.cpp
@@ -1,10 +1,10 @@
 #define BOOST_TEST_MAIN
 #include <stdexcept>
+#include <memory>
 #include <boost/test/included/unit_test.hpp>
 #include "circle.hpp"
 #include "rectangle.hpp"
 #include "composite-shape.hpp"
-#include <memory>
 
 const double EPSILON = 0.0001;
 
