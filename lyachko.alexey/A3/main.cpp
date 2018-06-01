66b42d303b3dc7236afadd747d99080f92bbb501
Alexey Lyachko <alloverffs@gmail.com>
2018-06-01 17:56:36 +0300

    Mains Changed

--- lyachko.alexey/A3/main.cpp
+++ lyachko.alexey/A3/main.cpp
@@ -9,33 +9,52 @@ int main()
 
   try
   {
-
+    std::cout << " Creating compositeshape based on rectangle1 " << std::endl;
     auto rectangle1 = std::shared_ptr<lyachko::Shape>(new lyachko::Rectangle({{1,1},2,2}));
     auto rectangle2 = std::shared_ptr<lyachko::Shape>(new lyachko::Rectangle({{3,3},2,2}));
 
     lyachko::CompositeShape compositeshape( rectangle1 );
 
+    std::cout << " CompositeShape's x " << compositeshape.getFrameRect().pos.x << std::endl;
+    std::cout << " CompositeShape's y " << compositeshape.getFrameRect().pos.y << std::endl;
+    std::cout << " CompositeShape's width " << compositeshape.getFrameRect().width << std::endl;
+    std::cout << " CompositeShape's height " << compositeshape.getFrameRect().height << std::endl;
+
     compositeshape.add( rectangle2 );
 
-    std::cout << compositeshape.getFrameRect().pos.x << std::endl;
-    std::cout << compositeshape.getFrameRect().pos.y << std::endl;
-    std::cout << compositeshape.getFrameRect().width << std::endl;
-    std::cout << compositeshape.getFrameRect().height << std::endl;
+    std::cout << " Adding Rectagnle2 to compositeshape " << std::endl;
+
+    std::cout << " CompositeShape's x "  << compositeshape.getFrameRect().pos.x << std::endl;
+    std::cout << " CompositeShape's y " << compositeshape.getFrameRect().pos.y << std::endl;
+    std::cout << " CompositeShape's width " << compositeshape.getFrameRect().width << std::endl;
+    std::cout << " CompositeShape's height " << compositeshape.getFrameRect().height << std::endl;
 
     compositeshape.move( { 10, 10 } );
 
-    std::cout << compositeshape.getFrameRect().pos.x << std::endl;
-    std::cout << compositeshape.getFrameRect().pos.y << std::endl;
-    std::cout << compositeshape.getFrameRect().width << std::endl;
-    std::cout << compositeshape.getFrameRect().height << std::endl;
+    std::cout << " Moving to 10; 10 " << std::endl;
+
+    std::cout << " CompositeShape's x " << compositeshape.getFrameRect().pos.x << std::endl;
+    std::cout << " CompositeShape's y " << compositeshape.getFrameRect().pos.y << std::endl;
+    std::cout << " CompositeShape's width " << compositeshape.getFrameRect().width << std::endl;
+    std::cout << " CompositeShape's height " << compositeshape.getFrameRect().height << std::endl;
 
     compositeshape.move( -1, 1 );
 
-    std::cout << compositeshape.getFrameRect().pos.x << std::endl;
-    std::cout << compositeshape.getFrameRect().pos.y << std::endl;
-    std::cout << compositeshape.getFrameRect().width << std::endl;
-    std::cout << compositeshape.getFrameRect().height << std::endl;
+    std::cout << " Shifting by -1, 1 " << std::endl;
 
+    std::cout << " CompositeShape's x " << compositeshape.getFrameRect().pos.x << std::endl;
+    std::cout << " CompositeShape's y " << compositeshape.getFrameRect().pos.y << std::endl;
+    std::cout << " CompositeShape's width " << compositeshape.getFrameRect().width << std::endl;
+    std::cout << " CompositeShape's height " << compositeshape.getFrameRect().height << std::endl;
+
+    compositeshape.remove(1);
+
+    std::cout << " Removing first element " << std::endl;
+
+    std::cout << " CompositeShape's x " << compositeshape.getFrameRect().pos.x << std::endl;
+    std::cout << " CompositeShape's y " << compositeshape.getFrameRect().pos.y << std::endl;
+    std::cout << " CompositeShape's width " << compositeshape.getFrameRect().width << std::endl;
+    std::cout << " CompositeShape's height " << compositeshape.getFrameRect().height << std::endl;
   
   } catch ( std::invalid_argument & smth ) 
   {
--- lyachko.alexey/A4/main.cpp
+++ lyachko.alexey/A4/main.cpp
@@ -9,6 +9,7 @@ int main()
 
   try
   {
+    std::cout << " Creating 4 figures to be added to composite shape and one to test matrix class " << std::endl;
     std::shared_ptr<lyachko::Shape> rectangle1(new lyachko::Rectangle({{1,1},1,1}));
     std::shared_ptr<lyachko::Shape> circle2(new lyachko::Circle({9,9},2));
     std::shared_ptr<lyachko::Shape> rectangle3(new lyachko::Rectangle({{1,1},2,2}));
@@ -26,9 +27,13 @@ int main()
     compositeshape.add(rectangle3);
     compositeshape.add(circle4);
 
+
     matrix = compositeshape.getMatrix();
     matrix2 = matrix;
     matrix2->info();
+
+    std::cout << " Adding element to the 3rd layer " << std::endl;
+
     matrix2->addElement(circle5);
     matrix2->info();
 
